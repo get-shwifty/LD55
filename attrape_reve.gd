e820ef16fc8590daba10a3f9d2a78dfc85ec0ceb
Alexis Schad <a@mandelbro.com>
2024-04-15 15:29:43 +0200

    dream catcher pos

--- attrape_reve.gd
+++ attrape_reve.gd
@@ -20,14 +20,10 @@ extends Node2D
 @onready var ropes_sprite = preload("res://ropes.tscn")
 @onready var nodes_sprite = preload("res://illuminated_nodes.tscn")
 
-var current_node = 0
 var illuminated_nodes = {}
 var shown_ropes = {}
 var discovery_step = 1
 
-enum Card {
-	CLOCKWISE, ANTICLOCKWISE, SYMMETRY, DOWN, TRANSFORM
-}
 const ROPE_TO_FRAME = {
 	"0 1" = 5,
 	"0 2" = 2,
@@ -47,16 +43,16 @@ func _ready():
 	#animation.play("AttrapeReveGrand2")
 	#animation.queue("AttrapeReveGrand3")
 	#animation.queue("AttrapeReveGrand4")
-	show_rope("1 2")
-	show_rope("1 4")
-	show_rope("0 3")
-	hide_rope("0 3")
-	set_discovery_step(2)
-	set_discovery_step(3)
-	show_node(0)
-	show_node(5)
-	show_node(6)
-	hide_node(5)
+	#show_rope("1 2")
+	#show_rope("1 4")
+	#show_rope("0 3")
+	#hide_rope("0 3")
+	#set_discovery_step(2)
+	#set_discovery_step(3)
+	#show_node(0)
+	#show_node(5)
+	#show_node(6)
+	#hide_node(5)
 	pass # Replace with function body.
 
 
@@ -69,19 +65,18 @@ func ensure_rope(rope):
 	if rope not in shown_ropes:
 		shown_ropes[rope] = ropes_sprite.instantiate()
 		shown_ropes[rope].frame = ROPE_TO_FRAME[rope]
+		shown_ropes[rope].modulate = Color(Color.WHITE, 0.0)
 		add_child(shown_ropes[rope])
 
 func show_rope(rope):
 	ensure_rope(rope)
 
-	shown_ropes[rope].modulate = Color(Color.WHITE, 0.0)
 	var tween = get_tree().create_tween()
 	tween.tween_property(shown_ropes[rope], "modulate", Color(Color.WHITE, 1.0), 0.15)
 
 func hide_rope(rope):
 	ensure_rope(rope)
 
-	shown_ropes[rope].modulate = Color(Color.WHITE, 1.0)
 	var tween = get_tree().create_tween()
 	tween.tween_property(shown_ropes[rope], "modulate", Color(Color.WHITE, 0.0), 0.15)
 
@@ -95,18 +90,23 @@ func ensure_node(node):
 	if node not in illuminated_nodes:
 		illuminated_nodes[node] = nodes_sprite.instantiate()
 		illuminated_nodes[node].frame = NODE_TO_FRAME[node]
+		illuminated_nodes[node].modulate = Color(Color.WHITE, 0.0)
 		add_child(illuminated_nodes[node])
 
 func show_node(node):
 	ensure_node(node)
 
-	illuminated_nodes[node].modulate = Color(Color.WHITE, 0.0)
 	var tween = get_tree().create_tween()
 	tween.tween_property(illuminated_nodes[node], "modulate", Color(Color.WHITE, 1.0), 0.15)
 
 func hide_node(node):
 	ensure_node(node)
 
-	illuminated_nodes[node].modulate = Color(Color.WHITE, 1.0)
 	var tween = get_tree().create_tween()
 	tween.tween_property(illuminated_nodes[node], "modulate", Color(Color.WHITE, 0.0), 0.15)
+
+func clear_all():
+	for rope in shown_ropes.keys():
+		hide_rope(rope)
+	for node in illuminated_nodes.keys():
+		hide_node(node)
