a59013215f6decb11c7fc9015aa1936873ec0e9b
david <godicked@gmail.com>
2024-04-15 02:41:30 +0200

    first commit

---
+++ card_hand.gd
@@ -0,0 +1,138 @@
+extends Node2D
+class_name CardHand
+
+const CARD_SCENE = preload('res://game/carte.tscn');
+
+@export var is_hand: bool = false
+@export var nb = 6
+@export var selected_hand: CardHand
+@export var y_max_offset = 30
+var card_list = []
+
+
+var card_width = 56
+var min_dist = 15
+var max_dist = card_width -8
+var max_width = 200
+var positions = []
+
+
+var move_speed = 4
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+		
+func clear():
+	for card in card_list:
+		card.queue_free()
+	card_list.clear()
+	positions.clear()
+	
+func send_back():
+	if is_hand:
+		return
+	for card in card_list:
+		card.send_back()
+	card_list.clear()
+
+
+func get_cards():
+	clear()
+	for i in range(randi_range(10,10)):
+		add_card()
+	if selected_hand:
+		selected_hand.clear()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if is_hand:
+		if Input.is_action_just_pressed("enter"):
+			get_cards()
+	else:
+		if Input.is_action_just_pressed("escape"):
+			send_back()
+	
+	for i in card_list.size():
+		var card = card_list[i]
+		card.position = card.position.move_toward(positions[i], move_speed)
+		
+	trigger_card_selection()
+
+
+func select_card(card):
+	var clone = CARD_SCENE.instantiate()
+	clone.card_type = card.card_type
+	clone.state = 99
+	clone.original = card
+	var pos = card.global_position
+	add_child(clone)
+	clone.global_position = pos
+	card_list.append(clone)
+	compute_positions()
+	
+func trigger_card_selection():
+	if not is_hand:
+		return
+	for card in card_list:
+		if card.trigger_select:
+			card.trigger_select = false
+			selected_hand.select_card(card)
+
+
+func add_card():
+	var card = CARD_SCENE.instantiate()
+	card.card_type = randi_range(0,6)
+	#print(card.card_type)
+	add_child(card)
+	if card_list.size() == 0:
+		card.position = Vector2(0,0)
+	else:
+		var last_card = card_list[card_list.size()-1]
+		card.position = last_card.position + Vector2(max_dist, 0)
+	card_list.append(card)
+	compute_positions()
+
+func compute_positions():
+	var n = card_list.size()
+	var min_width = card_width + ((n-1) * min_dist)
+	var card_dist = min_dist
+	var bonus_width = max_width - min_width
+	if n > 1 && bonus_width > 0:
+		card_dist = min(card_dist + bonus_width / (n-1), max_dist)
+	
+	var final_width = card_width + ((n-1) * card_dist)
+	print(final_width)
+	
+	var offset = final_width / 2.0 - card_width/2
+	
+	var y_offsets = []
+	y_offsets.resize(n)
+	y_offsets.fill(0)
+	if n > 1:
+		#print('---start---')
+		var ratio_offset = 0
+		for i in range(n):
+			var dist_from_center = abs((n/2.0) - (i+0.5)) / (n/2.0)
+			var ratio = sqrt((1 - dist_from_center))
+			if i == 0:
+				ratio_offset = ratio
+			y_offsets[i] = (ratio-ratio_offset) * y_max_offset
+		#print('---end---')
+	
+	#print(y_offsets)
+	
+	positions.clear()
+	for i in range(n):
+		var p = -offset + (i * card_dist)
+		positions.append(Vector2(p, -y_offsets[i]))
+		
+	for i in range(card_list.size()):
+		var card = card_list[i]
+		if i < card_list.size()-1:
+			card.update_shape(min(card_dist, card.total_width))
+		#else:
+			#card.update_shape(card.total_width)
+		
+	#print(positions[0])
---
+++ game/card.gd
@@ -0,0 +1,83 @@
+extends Node2D
+class_name Card
+
+@export var card_type: int = 0
+
+var total_width = 56
+
+# 0 available 1 hover 2 selected 3 unselect
+var state = 0
+var trigger_select = false
+
+var original: Card = null
+var speed = 4
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	$AnimatedSprite2D.frame = card_type
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if state == 3:
+		move_back()
+	
+func update_shape(visible_width: int):
+	var shape = $Area2D/CollisionShape2D
+	var width_diff = total_width - visible_width
+	shape.position = Vector2(-width_diff/2, 0)
+	shape.shape.size = Vector2(visible_width, shape.shape.size.y)
+	
+func hover():
+	if state != 0:
+		return
+	state = 1
+	modulate = Color(0,1,1,1)
+	z_index = 99
+	$AnimationPlayer.play("up")
+	
+func stop_hover():
+	if state != 1:
+		return
+	state = 0
+	modulate = Color(1,1,1,1)
+	z_index = 0
+	$AnimationPlayer.play_backwards("up")
+	
+func set_available():
+	state = 0
+	modulate = Color(1,1,1,1)
+	z_index = 0
+	
+func select():
+	if state != 1:
+		return
+	stop_hover()
+	state = 2
+	trigger_select = true
+	modulate = Color(1,1,1,0.4)
+	
+func send_back():
+	if not original:
+		return
+	state = 3
+	
+func move_back():
+	global_position = global_position.move_toward(original.global_position, speed)
+	if global_position == original.global_position:
+		original.set_available()
+		queue_free()
+
+
+func _on_area_2d_mouse_entered():
+	hover()
+
+
+func _on_area_2d_mouse_exited():
+	stop_hover()
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event.is_pressed():
+		select()
